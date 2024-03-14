02a8dff8801a48c2b3cb6d33fa12208523c759c1
Ashish Wasake <ashishwasake4381@gmail.com>
2024-03-14 17:18:16 +0530

    remove unwanted files

--- Grocery/src/main/java/com/grocery/api/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grocery.api.repository;
-
-import com.grocery.api.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
-}
