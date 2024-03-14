7de3bd6ef9df448738abcce4b959ee0ae7dee049
Ashish Wasake <140788138+ashishwasake@users.noreply.github.com>
2024-03-14 22:39:39 +0530

    Update UserNotFoundException.java

--- Grocery/src/main/java/com/grocery/api/customexception/UserNotFoundException.java
+++ Grocery/src/main/java/com/grocery/api/customexception/UserNotFoundException.java
@@ -2,6 +2,8 @@ package com.grocery.api.customexception;
 
 public class UserNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public UserNotFoundException(Long userId) {
 		super("User with ID " + userId + " not found.");
 	}
